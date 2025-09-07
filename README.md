ff60b5ab4ba45b49de606468f2e6f9d568ed3f3d
Fernando Contreras <fernandomorann999@gmail.com>
2025-09-07 01:22:19 -0600

    add: configuracion readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# TAREAS---DESARROLLO-WEB-I
-REPOSITORIO PARA SUBIR TODAS LAS TAREAS ASIGNADAS EN EL PORTAL 
+# Migraciones
+---
+**Modelo 1**
+![Modelo 1 ](/diagramas/model1.png)
+---
+**Modelo 2**
+![Modelo 2 ](/diagramas/model2.png)
+---
+**Modelo 3**
+![Modelo 3 ](/diagramas/model3.png)
+---
+**Modelo 4**
+![Modelo 4 ](/diagramas/model4.png)
+---
--- libreria/admin.py
+++ libreria/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 
-
+from .models import Libro,Autor, Capitulo,Seccion
 # Register your models here.
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+	pass
+@admin.register(Capitulo)
+class CapituloAdmin(admin.ModelAdmin):
+	pass
+@admin.register(Seccion)
+class SeccionAdmin(admin.ModelAdmin):
+	pass
