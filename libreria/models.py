1a35898a70c24d8c693a31a421339ce7c93be3ca
Fernando Contreras <fernandomorann999@gmail.com>
2025-09-07 01:07:40 -0600

    feat: Primer modelo de la base de datos

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    nacionality = models.CharField(max_length=50)
