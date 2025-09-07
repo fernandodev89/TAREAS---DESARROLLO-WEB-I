4485a2ac221fef1bd22deedbd8258040592c3d11
Fernando Contreras <fernandomorann999@gmail.com>
2025-09-07 01:14:28 -0600

    feat: agregamos ultimo modelo

--- libreria/models.py
+++ libreria/models.py
@@ -13,3 +13,7 @@ class Libro(models.Model):
 class Capitulo(models.Model):
     titulo = models.CharField(max_length=100)
     libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+
+class Seccion(models.Model):
+    nombre = models.CharField(max_length=100)
+    capitulo = models.ForeignKey(Capitulo, on_delete=models.CASCADE)
